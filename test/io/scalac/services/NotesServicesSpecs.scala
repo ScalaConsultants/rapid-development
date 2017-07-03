836b2d5f06621e778e10dcaef88111ca6fa944d8
Leszek Gruchała <leszek@gruchala.eu>
2017-07-03 15:32:20 +0200

    Add NotesServices.update specs

--- test/io/scalac/services/NotesServicesSpecs.scala
+++ test/io/scalac/services/NotesServicesSpecs.scala
@@ -10,7 +10,7 @@ import org.joda.time.DateTime
 import io.scalac.common.BaseUnitTest
 import io.scalac.common.adapters.NotesDaoAdapter
 import io.scalac.common.play.Pagination
-import io.scalac.common.services.{DatabaseCallFailed, DatabaseResponse, InvalidResource, ServiceFailed}
+import io.scalac.common.services.{DatabaseCallFailed, DatabaseResponse, InvalidResource, MissingResource, ServiceFailed}
 import io.scalac.domain.entities.Note
 
 class NotesServicesSpecs extends BaseUnitTest {
@@ -31,7 +31,7 @@ class NotesServicesSpecs extends BaseUnitTest {
       "a note has been found for given id" in new Fixture {
         override def notesDao: NotesDaoAdapter = new NotesDaoAdapter() {
           override def find(noteId: UUID): DatabaseResponse[Option[Note]] =
-            Task.now(note.some.asRight)
+            Task.now(dbNote.some.asRight)
         }
 
         val response = notesService.find(uuid).runAsync.block
@@ -70,7 +70,7 @@ class NotesServicesSpecs extends BaseUnitTest {
       "notes have been found for given id" in new Fixture {
         override def notesDao: NotesDaoAdapter = new NotesDaoAdapter() {
           override def listAll(pagination: Pagination): DatabaseResponse[Seq[Note]] =
-            Task.now(Seq(note).asRight)
+            Task.now(Seq(dbNote).asRight)
         }
 
         val response = notesService.list(pagination).runAsync.block
@@ -101,7 +101,7 @@ class NotesServicesSpecs extends BaseUnitTest {
             Task.now(uuid.asRight)
         }
 
-        val newNote = IncomingNote(note.creator, note.note)
+        val newNote = IncomingNote(dbNote.creator, dbNote.note)
 
         val response = notesService.create(newNote).runAsync.block
         response.isRight shouldBe true
@@ -114,7 +114,7 @@ class NotesServicesSpecs extends BaseUnitTest {
       "provided note has too short creator field" in new Fixture {
         override def notesDao: NotesDaoAdapter = new NotesDaoAdapter()
 
-        val newNote = IncomingNote("A", note.note)
+        val newNote = IncomingNote("A", dbNote.note)
 
         val response = notesService.create(newNote).runAsync.block
         response.isLeft shouldBe true
@@ -124,7 +124,7 @@ class NotesServicesSpecs extends BaseUnitTest {
       "provided note has too short note field" in new Fixture {
         override def notesDao: NotesDaoAdapter = new NotesDaoAdapter()
 
-        val newNote = IncomingNote(note.creator, "")
+        val newNote = IncomingNote(dbNote.creator, "")
 
         val response = notesService.create(newNote).runAsync.block
         response.isLeft shouldBe true
@@ -136,7 +136,83 @@ class NotesServicesSpecs extends BaseUnitTest {
           override def create(note: Note): DatabaseResponse[UUID] =
             Task.now(DatabaseCallFailed("BOOM").asLeft)
         }
-        val newNote = IncomingNote(note.creator, note.note)
+        val newNote = IncomingNote(dbNote.creator, dbNote.note)
+
+        val response = notesService.create(newNote).runAsync.block
+        response.isLeft shouldBe true
+        response shouldBe ServiceFailed("BOOM").asLeft
+      }
+    }
+  }
+
+  "NotesService.update" should {
+    "update note successfully" when {
+      "existing note of given ID is found and valid note is provided" in new Fixture {
+        override def notesDao: NotesDaoAdapter = new NotesDaoAdapter() {
+          override def find(noteId: UUID): DatabaseResponse[Option[Note]] =
+            Task.now(dbNote.some.asRight)
+
+          override def update(note: Note): DatabaseResponse[Note] =
+            Task.now(note.copy(version = dbNote.version.map(_ + 1)).asRight)
+        }
+
+        val noteToUpdate = IncomingNote(dbNote.creator + "Superman", dbNote.note)
+        val updateNote = UpdateNote(uuid, noteToUpdate)
+
+        val response = notesService.update(updateNote).runAsync.block
+        response.isRight shouldBe true
+        val updatedNote = response.right.get
+        assert(updatedNote.recentEdit.isAfter(dbNote.updatedAt), "Recent update timestamp must be later than the one already persisted")
+        updatedNote.creator shouldBe noteToUpdate.creator
+        updatedNote.note shouldBe noteToUpdate.note
+        updatedNote.id shouldBe updateNote.id
+      }
+    }
+
+    "return failed response and not update a note" when {
+
+      "provided note has too short creator field" in new Fixture {
+        override def notesDao: NotesDaoAdapter = new NotesDaoAdapter()
+
+        val noteToUpdate = IncomingNote("BC", dbNote.note)
+        val updateNote = UpdateNote(uuid, noteToUpdate)
+
+        val response = notesService.update(updateNote).runAsync.block
+        response.isLeft shouldBe true
+        response shouldBe InvalidResource(Seq("creator's length must be between 3 and 100 characters")).asLeft
+      }
+
+      "provided note has empty short note field" in new Fixture {
+        override def notesDao: NotesDaoAdapter = new NotesDaoAdapter()
+
+        val noteToUpdate = IncomingNote(dbNote.creator, "")
+        val updateNote = UpdateNote(uuid, noteToUpdate)
+
+        val response = notesService.update(updateNote).runAsync.block
+        response.isLeft shouldBe true
+        response shouldBe InvalidResource(Seq("note's length must be between 1 and 5000 characters")).asLeft
+      }
+
+      "existing note of given ID is not found" in new Fixture {
+        override def notesDao: NotesDaoAdapter = new NotesDaoAdapter() {
+          override def find(noteId: UUID): DatabaseResponse[Option[Note]] =
+            Task.now(none.asRight)
+        }
+
+        val noteToUpdate = IncomingNote(dbNote.creator + "Superman", dbNote.note)
+        val updateNote = UpdateNote(uuid, noteToUpdate)
+
+        val response = notesService.update(updateNote).runAsync.block
+        response.isRight shouldBe false
+        response shouldBe MissingResource("Cannot update non-existent element").asLeft
+      }
+
+      "db returns an error" in new Fixture {
+        override def notesDao: NotesDaoAdapter = new NotesDaoAdapter() {
+          override def create(note: Note): DatabaseResponse[UUID] =
+            Task.now(DatabaseCallFailed("BOOM").asLeft)
+        }
+        val newNote = IncomingNote(dbNote.creator, dbNote.note)
 
         val response = notesService.create(newNote).runAsync.block
         response.isLeft shouldBe true
@@ -147,8 +223,8 @@ class NotesServicesSpecs extends BaseUnitTest {
 
   trait Fixture {
     val uuid = UUID.randomUUID()
-    val note = Note(Some(uuid), "creator", "some not so long note", DateTime.now(), DateTime.now(), Some(1))
-    val outgoingNote = OutgoingNote(note.id.get, note.creator, note.note, note.updatedAt)
+    val dbNote = Note(Some(uuid), "creator", "some not so long note", DateTime.now(), DateTime.now(), Some(1))
+    val outgoingNote = OutgoingNote(dbNote.id.get, dbNote.creator, dbNote.note, dbNote.updatedAt)
     val pagination = Pagination(limit = 10, offset = 0)
 
     def notesDao: NotesDaoAdapter
@@ -156,5 +232,3 @@ class NotesServicesSpecs extends BaseUnitTest {
     val notesService = new DefaultNotesService(notesDao)
   }
 }
-
-
