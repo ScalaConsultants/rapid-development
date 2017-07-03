d44e472eba67399611d6d2de35de6bbc48bc5eae
Leszek Gruchała <leszek@gruchala.eu>
2017-07-03 14:26:36 +0200

    Add logback for unit test, some small refactoring

--- test/io/scalac/common/BaseUnitTest.scala
+++ test/io/scalac/common/BaseUnitTest.scala
@@ -1,13 +1,19 @@
 package io.scalac.common
 
+import monix.eval.Task
 import org.scalatest.{Matchers, WordSpec}
 
 import io.scalac.common.logger.AppLogger
-import io.scalac.common.services.NoopServiceProfiler
+import io.scalac.common.services.{Context, Logger, NoopServiceProfiler, Profiler, Service, ServiceError}
 
 trait BaseUnitTest extends WordSpec with Matchers with FuturesSupport {
 
+  implicit val scheduler = monix.execution.Scheduler.Implicits.global
   implicit val serviceContext: auth.ServiceContext = auth.EmptyContext()
   implicit val serviceProfiler: services.ServiceProfiler = NoopServiceProfiler
   implicit val logger: AppLogger = AppLogger(getClass)
+
+  def mockService[Req, Res, E <: ServiceError](fn: Req => Task[Either[E, Res]]): Service[Req, Res, E] = new Service[Req, Res, E] {
+    def apply[Ctx: Context, P: Profiler, L: Logger](req: Req): Task[Either[E, Res]] = fn(req)
+  }
 }
--- test/io/scalac/services/NotesServicesSpecs.scala
+++ test/io/scalac/services/NotesServicesSpecs.scala
@@ -2,13 +2,14 @@ package io.scalac.services
 
 import java.util.UUID
 
-import monix.eval.Task
 import cats.syntax.either._
+import cats.syntax.option._
+import monix.eval.Task
 import org.joda.time.DateTime
 
 import io.scalac.common.BaseUnitTest
 import io.scalac.common.adapters.NotesDaoAdapter
-import io.scalac.common.services.{DatabaseCallFailed, DatabaseResponse, NoopServiceProfiler, ServiceFailed}
+import io.scalac.common.services.{DatabaseCallFailed, DatabaseResponse, ServiceFailed}
 import io.scalac.domain.entities.Note
 
 class NotesServicesSpecs extends BaseUnitTest {
@@ -18,23 +19,23 @@ class NotesServicesSpecs extends BaseUnitTest {
       "no note has been found for given id" in new Fixture {
         override def notesDao: NotesDaoAdapter = new NotesDaoAdapter() {
           override def find(noteId: UUID): DatabaseResponse[Option[Note]] =
-            Task.now(None.asRight)
+            Task.now(none.asRight)
         }
 
         val response = notesService.find(uuid).runAsync.block
         response.isRight shouldBe true
-        response shouldBe None.asRight
+        response shouldBe none.asRight
       }
 
       "a note has been found for given id" in new Fixture {
         override def notesDao: NotesDaoAdapter = new NotesDaoAdapter() {
           override def find(noteId: UUID): DatabaseResponse[Option[Note]] =
-            Task.now(Some(note).asRight)
+            Task.now(note.some.asRight)
         }
 
         val response = notesService.find(uuid).runAsync.block
         response.isRight shouldBe true
-        response shouldBe note.asRight
+        response shouldBe outgoingNote.some.asRight
       }
     }
 
@@ -56,10 +57,11 @@ class NotesServicesSpecs extends BaseUnitTest {
   trait Fixture {
     val uuid = UUID.randomUUID()
     val note = Note(Some(uuid), "creator", "some not so long note", DateTime.now(), DateTime.now(), Some(1))
+    val outgoingNote = OutgoingNote(note.id.get, note.creator, note.note, note.updatedAt)
 
     def notesDao: NotesDaoAdapter
 
-    val notesService = new DefaultNotesService(notesDao, new NoopServiceProfiler())
+    val notesService = new DefaultNotesService(notesDao)
   }
 }
 
